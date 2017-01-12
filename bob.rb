a28a159dedda16247c12504e6f68b9b055256747
jags2-0 <jessica@boomtownroi.com>
2017-01-12 17:07:46 -0500

    homework - test driven dev

--- bob.rb
+++ bob.rb
@@ -1,15 +1,45 @@
 class Bob
 
   def initialize
-
+    @special_characters = ["-", "!", ",","?","'"]
   end
 
   def hey (i_say)
-    if i_say == 'Tom-ay-to, tom-aaaah-to.'
-      'Whatever'
-    elsif i_say == "WATCH OUT!"
-      'Whoa, chill out!'
+    phrase = i_say.chars
+
+    phrase.any? do |n|
+      if @special_characters.include? n
+        return 'Whatever.'
+      end
+
+    start = phrase.first
+      if start == start.upcase
+        return 'Whoa, chill out!'
+      end
+
+      # if i_say.end_with? '?'
+      #   return 'Sure.'
+      # end
+      #
+      # if i_say == ' '
+      #   return 'Fine. Be that way!'
+      # end
+
     end
+
+
+
+
   end
 
 end
+
+
+
+# bob says
+# 'Whatever.' - special characters
+
+
+# 'Whoa, chill out!' -
+# 'Fine. Be that way!' - spaces
+
